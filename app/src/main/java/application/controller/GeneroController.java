490ab39a6ae919cd0eb79810cd4cc90f8ea39ba6
Beatriz <BEATRIZ.CRUZ84@ETEC.SP.GOV.BR>
2024-05-03 10:43:54 +0000

    save1

--- app/src/main/java/application/controller/GeneroController.java
+++ app/src/main/java/application/controller/GeneroController.java
@@ -2,6 +2,7 @@ package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import application.repository.GeneroRepository;
@@ -11,4 +12,9 @@ import application.repository.GeneroRepository;
 public class GeneroController {
     @Autowired
     private GeneroRepository generoRepo;
+
+    @GetMapping
+    public String insert() {
+        return "/WEB-INF/generos/insert.jsp";
+    }
 }
